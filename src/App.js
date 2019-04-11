7376e61b933cc37c72604641c18f8d4f144ff850
jennawills <jenna.wills321@gmail.com>
2019-04-10 22:50:58 -0400

    updated about me css background and color

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Uv from "./uvtracknew.jpg";
 import Hoa from "./hoa.jpg";
 
 
-
+var showDescInd = false;
+var showDescId = 0;
 
 var projects = [
-  {id: 0, name: "Task Board", image:Task},
-  {id: 1, name: "HOA", image:Hoa},
-  {id: 2, name: "UV Index App", image:Uv},
+  {id: 0, name: "Task Board", image:Task, description: "Built using React, this is a task board based on a Kanban board. Create a new card, add or remove a task item."},
+  {id: 1, name: "HOA", image:Hoa, description: "Built using React, this is a sample site for a community homeowner association. Uses routing to navigate to additional site pages."},
+  {id: 2, name: "UV Index App", image:Uv, description: "Built using React, a single page app that uses API from OpenUV to fetch realtime UV index. Also, as a fun feature, user is able to choose from different backgrounds."},
 //  {id: 3, name: "Arcade Game"},
 //  {id: 4, name: "Neighborhood App"},
 //  {id: 5, name: "Memory Game"}
@@ -37,6 +38,20 @@ var lettersHalf = [
 
 class App extends Component {
 
+  constructor (props) {
+    super(props);
+    this.scrollDiv = React.createRef();
+    this.state = {showDescInd,showDescId};
+  }
+
+
+  showDescription = (event) => {
+      this.setState({showDescId:event.target.id, showDescInd:true})
+  }
+
+  closeDescription = (event) => {
+    this.setState({showDescInd:false})
+  }
 
   render() {
 
@@ -46,7 +61,9 @@ class App extends Component {
         <header />
         <div className="introduction">
           <div className="lettersContainer">
-            <div className="lettersContainer_inner">
+            <div className="lettersContainer_inner" onClick={() => {
+            this.scrollDiv.current.scrollIntoView({ behavior: 'smooth' });
+          }}>
               {letters[0].letter.map(item =>
                 item !== " " ?
                 <div className="letters"> {item} </div>:
@@ -144,7 +161,7 @@ class App extends Component {
 
           </div>
         </div>
-        <div className="sectionTwo">
+        <div className="sectionTwo" ref={this.scrollDiv}>
           <div className="aboutMe">
             <h1 className="aboutMe_header"> A b o u t Me </h1>
             <p className="aboutMe_content">
@@ -161,10 +178,16 @@ class App extends Component {
         </div>
         <div className="portfolio">
           <h1 className="portfolio_header"> Portfolio </h1>
-          <ul className="portfolio_list">
+
+        {this.state.showDescInd === true &&  <div className="portfolioProject">
+          <button onClick={this.closeDescription}> Close </button>
+            <p>{projects[this.state.showDescId].description}</p>
+            <img src={projects[this.state.showDescId].image} alt={projects[this.state.showDescId].name}/>
+          </div>}
+          <ul className="portfolio_list" onClick={this.showDescription}>
             {projects.map(item => (
               <li id={item.id} className="portfolio_items">
-                <img src={item.image} alt={item.name} className="portfolio_list_images" />
+                <img id={item.id} src={item.image} alt={item.name} className="portfolio_list_images" />
               </li>
             ))}
           </ul>
